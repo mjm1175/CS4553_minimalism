887eced4c94b985da32dc67273ab03122289c6bb
Maria <mjm1175@nyu.edu>
2022-02-02 21:49:14 -0500

    color changing obstacles working

--- Assets/Scripts/ObstacleColor.cs
+++ Assets/Scripts/ObstacleColor.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class ObstacleColor : MonoBehaviour
 {
+    public bool dangerous = false;      // orange when true, yellow when false
+    private SpriteRenderer thisRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        thisRenderer = GetComponent<SpriteRenderer>();
+        StartCoroutine(CycleColors());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator CycleColors(){
+        while (true){
+            if (dangerous){
+                thisRenderer.color = new Color(0.973f,1.0f,0.0f,1.0f);
+                dangerous = false;
+            } else {
+                thisRenderer.color = new Color(1.0f,0.616f,0.0f,1.0f);
+                dangerous = true;
+            }
+            yield return new WaitForSeconds(2.0f);
+        }
     }
 }
--- Assets/Scripts/ObstacleHit.cs
+++ Assets/Scripts/ObstacleHit.cs
@@ -19,12 +19,18 @@ public class ObstacleHit : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Obstacle")){
+
+            ObstacleColor obsColor = other.GetComponent<ObstacleColor>();
+
             // TODO: DECIDE IF THIS MAKES SENSE TO INCLUDE
             // keeping you from dying if obstacle is too low to shoot
-            if (other.transform.position.y >= -2.06f){
+                //if (other.transform.position.y >= -2.06f){
+            if (obsColor.dangerous){
+
                 gameObject.SetActive(false);
 
                 restartMenu.SetActive(true);                
+                //}                
             }
         }
     }
